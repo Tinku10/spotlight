424900138cfcff618227301c1f11f307db5d117f
Tinku10 <tinku.kvs@gmail.com>
2021-05-28 14:18:05 +0530

    minor changes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('muzix app is running!');
+    expect(await page.getTitleText()).toEqual('spotlight app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'muzix'`, () => {
+  it(`should have as title 'spotlight'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('muzix');
+    expect(app.title).toEqual('spotlight');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('muzix app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('spotlight app is running!');
   });
 });
